2895f17288391b9d205473bbad980636af7fdf26
yahagi_day <yahagi.day@tensi.org>
2018-06-13 21:48:32 +0900

    サイネージページの作成

--- signage/index.php
+++ signage/index.php
@@ -7,6 +7,7 @@
         
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/css/materialize.min.css">
         <link rel="stylesheet" type="text/css" href="style.css">
+        <script type="text/javascript" src="../config/footerFixed.js"></script>
 
         <!-- Search Engine Block -->
         <meta name="robots" content="noindex,nofollow" />
@@ -14,9 +15,13 @@
         <title>サイネージ - Ticper</title>
     </head>
     <body>
-    	<h1>団体名</h1><br>
-        <h2>食品名</h2><br>
-        <h2 class="price">値段</h2><h2 class="stock">在庫</h2>
+        <div id="main">
+    	   <h1>団体名</h1><
+            <h1>住所</h1><br>
+            <h2 class="food">食品名</h2><br>
+            <h2 class="price">値段</h2>
+            <h2 class="stock">在庫</h2>
+        </div>        
     </body>
+    <div id=footer>食品の説明</div>
 </html>
-        
