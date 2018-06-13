de40d76adaad7bfe3e0014d2eedb17c8da0aba81
yahagi_day <yahagi.day@tensi.org>
2018-06-13 18:51:04 +0900

    サイネージの作成

---
+++ signage/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html charset="UTF-8">
+    <head>
+        <!-- System Properties -->
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/css/materialize.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <!-- Search Engine Block -->
+        <meta name="robots" content="noindex,nofollow" />
+        <!-- Title -->
+        <title>サイネージ - Ticper</title>
+    </head>
+    <body>
+    	<h1>団体名</h1><br>
+        <h2>食品名</h2><br>
+        <h2 class="price">値段</h2><h2 class="stock">在庫</h2>
+    </body>
+</html>
+        
