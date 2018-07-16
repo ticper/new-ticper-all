59d84e1f9f8faef6984060be4d9c5853d5b8b57e
Ryo Nakagome <nakagome@tento-net.com>
2018-07-16 23:58:33 +0900

    調理依頼画面を作成

--- f-request.php
+++ f-request.php
@@ -55,15 +55,24 @@
                 <div class="col s12">
                     <?php
                         require_once('config/config.php');
-                        $sql = mysqli_query($db_link, "SELECT UserName FROM tp_user_all WHERE UserID = '$UserID'");
-                        $result = mysqli_fetch_assoc($sql);
-                    ?>
-                    <h2>ようこそ</h2>
-                    <p>こんにちは、<?php print($result['UserName']); ?>さん、今回は何をしますか？
-                    <table border="2">
-                        <tr><td style="font-size: x-large;"><a href="l-list.php"><b>落とし物管理</b></a></td><td style="font-size: x-large;"><a href="signage.php"><b>サイネージ操作</b></a></td></tr>
-                        <tr><td style="font-size: x-large;"><a href="f-request.php"><b>調理依頼</b></a></td></tr>
-                    </table>
+                        $sql = mysqli_query($db_link, "SELECT OrgID, OrgName FROM tp_org");
+                        while($result = mysqli_fetch_assoc($sql)) {
+                            print('<h3>'.$result['OrgName'].'</h3>');
+                            print('<table>');
+                            $orgid = $result['OrgID'];
+                            $sql2 = mysqli_query($db_link, "SELECT FoodID, FoodName FROM tp_food WHERE OrgID = '$orgid'");
+                            while($result2 = mysqli_fetch_assoc($sql2)) {
+                                print('<tr><th>'.$result2['FoodName'].'</th>');
+                                $foodid = $result2['FoodID'];
+                                $sql3 = mysqli_query($db_link, "SELECT count(*) AS num FROM tp_ticket WHERE FoodID = '$foodid' AND Requested = 0");
+                                while($result3 = mysqli_fetch_assoc($sql3)) {
+                                    print('<td><font size="5">'.$result3['num'].'</font>枚</td>');
+                                    print('<td><form action="f-requestdo.php" method="POST"><input type="hidden" name="foodid" value="'.$foodid.'"><input type="hidden" name="count" value="'.$result3['num'].'"><button type="submit" class="btn">'.$result3['num'].'枚の調理を依頼する</button></form></td></tr>');
+                                }
+                            }
+                            print('</table>');
+                        }
+                    ?>  
                 </div>
             </div>
         </div>
